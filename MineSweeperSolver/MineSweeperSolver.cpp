10da6963046eb796ec2592918c8869982016e93c
Matt Brown <gitmattb@gmail.com>
2020-10-29 08:14:37 -0700

    initial commit

---
+++ MineSweeperSolver/MineSweeperSolver.cpp
@@ -0,0 +1,18 @@
+﻿// MineSweeperSolver.cpp : Defines the entry point for the application.
+//
+
+#include "MineSweeperSolver.h"
+#include "../MineSweeperGame/MineSweeperGame.h"
+
+
+int main()
+{
+	MineSweeperGame game (10, 10, 1);
+	game.PrintBoard();
+	int in;
+	game.PrintBoard(true);
+	game.ViewSquareByCoords(5, 5);
+	game.PrintBoard();
+	std::cin >> in;
+	return 0;
+}
